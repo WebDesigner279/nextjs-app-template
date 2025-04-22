e565fc75b3a54a52954c48f2953c03da03afe941
tekraseg02 <tekraseg02@gmail.com>
2025-04-22 17:14:30 +0000

    atualização favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+// app/layout.tsx
 import "./globals.scss";
 import React from "react";
 
 export const metadata = {
   title: "Next.js App Template",
-  description:
-    "",
+  description: "",
+  icons: {
+    icon: "/favicon.ico", // ✅ favicon 
+  },
 };
 
 export default function RootLayout({
